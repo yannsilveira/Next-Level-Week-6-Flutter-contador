f97a074a61ad3935cca326cda77f185e31057633
Yannops <yannsilveira34@gmail.com>
2021-06-21 23:55:40 -0300

    Contador de clicks

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(
+      primarySwatch: Colors.purple
+    ),
+    home: HomePage()
+      ),
+  );
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment() {
+    count++;
+    setState(() {
+      
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'Meu primeiro App com Flutter')),
+      ),
+      body: Center( 
+        child: Text(
+          "Contador\n$count",
+           textAlign: TextAlign.center
+           )),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: (){
+              increment();
+            },
+          ),
+        );
+  }
+}
